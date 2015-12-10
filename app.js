c1e516858ad770dbdedbe47485705d5533499b53
JMARCHESENYC <justinmarchese11@gmail.com>
2015-12-10 18:10:40 -0500

    Almost there

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-angular.module('flapperNews', ['ui.router'])
+var app = angular.module('flapperNews', ['ui.router'])
 
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
 
-var app = angular.module('flapperNews', []);
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}]);
 
 app.factory('posts', [function(){
   var o = {
@@ -14,15 +26,10 @@ app.controller('MainCtrl', [
 '$scope',
 'posts',
 function($scope, posts){
+	$scope.test = 'Hello world!';
+	
 	$scope.posts = posts.posts;
-  $scope.test = 'Hello world!';
-  $scope.posts = [
-   {title: 'post 1', upvotes: 5},
-   {title: 'post 2', upvotes: 2},
-   {title: 'post 3', upvotes: 15},
-   {title: 'post 4', upvotes: 9},
-   {title: 'post 5', upvotes: 4}
-];
+  
 
 $scope.addPost = function(){
   if(!$scope.title || $scope.title === '') { return; }
@@ -40,6 +47,8 @@ $scope.incrementUpvotes = function(post) {
 };
 
 }]);
+
+
 
 
 
